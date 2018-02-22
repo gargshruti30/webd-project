b801d6ac56a44ac292b9fb38146fd58d697ac235
shradha <shradhakhapra1899@gmail.com>
2018-02-23 02:33:41 +0530

    passport

---
+++ hehe/finalproject/public/login.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+    // $('#btn2').on('click', function () {
+    //    let username=$('#inp1').val();
+    //     let password=$('#inp2').val();
+    //     let URL=`http://localhost:5000/login`;
+    //     $.post({
+    //         'url': URL,
+    //         'data': {username:username,password:password},
+    //         'success': function (res) {
+    //             // $("html").replaceWith(res);
+    //             console.log(res);
+    //
+    //         }
+    //     });
+    //
+    // })
+});
